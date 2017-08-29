9f6828f72cd3c90f30e9a6aac5b91f18bb2612fd
scyphio122 <konrad.traczyk122@gmail.com>
2017-08-29 12:23:13 +0200

    Added form to register user

--- public_html/login.js
+++ public_html/login.js
@@ -36,15 +36,3 @@ function submitLoginData()
     //httpGetAsync('/login?username='+_user+'&password='+_pass, onLoginFail());
 
 }
-
-
-function httpGetAsync(theUrl, callback)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous
-    xmlHttp.send(null);
-}
