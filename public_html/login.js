9e0c66d7c07c41398d686ee6997d1b9176ec5d17
scyphio122 <konrad.traczyk122@gmail.com>
2017-08-29 14:15:36 +0200

    Added registration form

--- public_html/login.js
+++ public_html/login.js
@@ -2,11 +2,31 @@ var userName;
 var loginButton;
 var loginForm;
 var idUser;
+var registrationModal;
+var registrationCloseButton;
 
 window.onload = function()
 {
 	loginForm = document.getElementById("loginForm");
 	loginButton = document.getElementById("loginButton");
+
+	registrationModal = document.getElementById("registration_modal");
+	// Get the <span> element that closes the modal
+	registrationCloseButton = document.getElementById("registrationModalClose");
+
+	// Close modal when user clics 'X' button
+	registrationCloseButton.onclick = function()
+	{
+		registrationModal.style.display = "none";
+	}
+
+	document.getElementById("closeRegistrationButton").onclick = function()
+	{
+		registrationModal.style.display = "none";
+	}
+
+	document.getElementById("registerUserButton").onclick = openRegistrationModal;
+
 }
 function onLoginSuccess(userId)
 {
@@ -35,4 +55,22 @@ function submitLoginData()
     //var _pass = document.getElementById("password");
     //httpGetAsync('/login?username='+_user+'&password='+_pass, onLoginFail());
 
+}
+
+
+//                     <!-- REGISTRATION -->
+// Opening the modal
+function openRegistrationModal()
+{
+	// Display the modal window
+	registrationModal.style.display = "block";
+}
+
+// Close the modal when user clicks outside of it
+window.onclick = function()
+{
+	if (event.target == registrationModal)
+	{
+		registrationModal.style.display = "none";
+	}
 }
