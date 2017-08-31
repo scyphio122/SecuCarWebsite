c4062dbf1b2f5c82f851b829bb634a180fa4d08e
scyphio122 <konrad.traczyk122@gmail.com>
2017-08-31 12:24:32 +0200

    Info box

--- public_html/map.js
+++ public_html/map.js
@@ -28,13 +28,29 @@ function addMarker(latitude, longtitude, sample)
 		 'Acceleration: ' + sample["acceleration"] + " m/s^2" + '<br>' +
 		 'Azimuth: ' + sample["azimuth"] +
 		 '</div>' +
+		 '<span class="icon-down-dir"></span>' +
 		"</div>";
 
-	var _infoWindow = new google.maps.InfoWindow(
-		{
-			content: contentWindowString
-		}
-	);
+var infoBoxOptions = {
+	content: contentWindowString,
+	distableAytoPan: false,
+	maxWidth: 0,
+	pixelOffset: new google.maps.Size(-140, -15)
+	,zIndex: null
+	,boxStyle: {
+		opacity: 11,
+		width: "280px",
+	}
+	,closeBoxMargin: "10px 2px 2px 2px"
+	,closeBoxURL: "https://www.google.com/intl/en_us/mapfiles/close.gif"
+	,infoBoxClearance: new google.maps.Size(1, 1)
+	,isHidden: false
+	,alignBottom: true
+	,pane: "floatPane"
+	,enableEventPropagation: false
+}
+
+	var _infoWindow = new InfoBox(infoBoxOptions);
 
 	var _icon = {
 		path: "M 500,10 C 229.4,10 10,229.4 10,500 10,770.6 229.4,990 500,990 770.6,990 990,770.6 990,500 990,229.4 770.6,10 500,10 Z M 286.4,826.5 462.2,500 286.4,173.5 824.9,500 286.4,826.5 Z",
@@ -60,62 +76,6 @@ function addMarker(latitude, longtitude, sample)
 		azimuth: sample["azimuth"]
 	});
 
-
-
-	/*
-	 * The google.maps.event.addListener() event waits for
-	 * the creation of the infowindow HTML structure 'domready'
-	 * and before the opening of the infowindow defined styles
-	 * are applied.
-	 */
-	google.maps.event.addListener(_infoWindow, 'domready', function() {
-
-		 // Reference to the DIV which receives the contents of the infowindow using jQuery
-		 var iwOuter = $('.gm-style-iw');
-
-		 /* The DIV we want to change is above the .gm-style-iw DIV.
-			* So, we use jQuery and create a iwBackground variable,
-			* and took advantage of the existing reference to .gm-style-iw for the previous DIV with .prev().
-			*/
-		 var iwBackground = iwOuter.prev();
-
-		 // Remove the background shadow DIV
-		 iwBackground.children(':nth-child(2)').css({'display' : 'none'});
-
-		 // Remove the white background DIV
-		 iwBackground.children(':nth-child(4)').css({'display' : 'none'});
-
-		 // Moves the infowindow 50px to the right.
-		 iwOuter.parent().parent().css({left: '50px'});
-
-		 // Moves the shadow of the arrow to the left margin
-		iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 41px !important;'});
-
-		// Moves the arrow to the left margin
-		iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 41px !important;'});
-
-		// Moves the arrow 76px to the left margin
-		iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'background-color: #565650 !important; margin-top: 11px !important;'});
-		iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'color: #565650; margin-top: 11px !important;'});
-
-		var iwCloseBtn = iwOuter.next();
-
-		// Apply the desired effect to the close button
-		iwCloseBtn.css({
-		  opacity: '1', // by default the close button has an opacity of 0.7
-		  right: '-10px', top: '40px', // button repositioning
-		  border: '7px solid #48b5e9', // increasing button border and new color
-		  'border-radius': '13px', // circular effect
-		  'box-shadow': '0 0 5px #3990B9' // 3D effect to highlight the button
-		  });
-
-		// The API automatically applies 0.7 opacity to the button after the mouseout event.
-		// This function reverses this event to the desired value.
-		iwCloseBtn.mouseout(function(){
-		  $(this).css({opacity: '1'});
-		});
-
-	});
 
 	_marker.addListener('click', function() {
 			_infoWindow.open(map, _marker);
